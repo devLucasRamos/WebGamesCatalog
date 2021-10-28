16b914218ed70129cb3d0bec5f09b560f7408b94
Lucas Ramos <87498874+devLucasRamos@users.noreply.github.com>
2021-10-28 12:05:38 -0300

    Criando Get,Post,Put,Patch e Delete

---
+++ Controllers/V1/GamesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGamesCatalog.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class GamesController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+            return Ok();
+        }
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<List<object>>> ObterByGuid(Guid idJogo)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<object>> InserirJogo(object jogo)
+        {
+            return Ok();
+        }
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo,object jogo)
+        {
+            return Ok();
+        }
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarPrecoJogo(Guid idJogo, double preco)
+        {
+            return Ok();
+        }
+        [HttpDelete("{idJogo:guid}")]
+        public async Task<AcceptedResult> ApagarJogo(Guid idJogo)
+        {
+            return Ok();
+        }
+    }
+}
